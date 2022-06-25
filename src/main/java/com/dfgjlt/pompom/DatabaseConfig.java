4d02bae3ccfe3bdfc4b27776369ccf00d6fb3298
Timo Nam <dwbh7358@gmail.com>
2022-06-26 01:45:23 +0900

    add: class DatabaseConfig

---
+++ src/main/java/com/dfgjlt/pompom/DatabaseConfig.java
@@ -0,0 +1,21 @@
+package com.dfgjlt.pompom;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
