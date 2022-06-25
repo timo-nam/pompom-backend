e83208647a7a881a792d5508e09fe0e5e6a81c47
Timo Nam <dwbh7358@gmail.com>
2022-06-25 17:16:53 +0900

    init: POMPOM backend with Spring

---
+++ src/main/java/com/dfgjlt/pompom/PompomApplication.java
@@ -0,0 +1,13 @@
+package com.dfgjlt.pompom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PompomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PompomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dfgjlt/pompom/PompomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfgjlt.pompom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PompomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
