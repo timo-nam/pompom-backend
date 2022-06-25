35ea760afe1605b5258c3d9a1ea60ad5375be3f2
Timo Nam <dwbh7358@gmail.com>
2022-06-25 17:39:24 +0900

    foobar: can I deploy this app with Heroku?

--- src/main/java/com/dfgjlt/pompom/PompomApplication.java
+++ src/main/java/com/dfgjlt/pompom/PompomApplication.java
@@ -2,9 +2,20 @@ package com.dfgjlt.pompom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class PompomApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "We are POMPOM.";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PompomApplication.class, args);
