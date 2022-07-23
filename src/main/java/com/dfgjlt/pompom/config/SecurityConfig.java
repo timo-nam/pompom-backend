39f2f19c991342247bb93862c5b59909bc49f09b
Timo Nam <dwbh7358@gmail.com>
2022-07-23 23:53:51 +0900

    add: SecurityConfig

--- src/main/java/com/dfgjlt/pompom/config/SecurityConfig.java
+++ src/main/java/com/dfgjlt/pompom/config/SecurityConfig.java
@@ -1,2 +1,20 @@
-package com.dfgjlt.pompom.config;public class SecurityConfig {
+package com.dfgjlt.pompom.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/api/hello").permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
 }
